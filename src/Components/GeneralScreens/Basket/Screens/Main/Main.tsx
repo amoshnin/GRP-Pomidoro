6ea1f8b52e34dffbe3fbedf61f707a9065087838
Artem Moshnin <62706319+Artem711@users.noreply.github.com>
2020-07-05 16:46:11 +0200

    Code refactoring

--- src/Components/GeneralScreens/Basket/Screens/Main/Main.tsx
+++ src/Components/GeneralScreens/Basket/Screens/Main/Main.tsx
@@ -10,6 +10,7 @@ import BonusesSection from "./BonusesSection/BonusesSection"
 import Footer from "./Footer/Footer"
 
 // EXTRA IMPORTS //
+import { removeDuplicates } from "~/Components/Shared/Helpers/RemoveDuplicatesFunction"
 
 /////////////////////////////////////////////////////////////////////////////
 
@@ -61,50 +62,6 @@ const Main: React.FC<PropsType> = (props) => {
       }, 0)
     )
   }, [props.OrderItemsList])
-
-  const removeDuplicates = (originalArray: Array<any>, prop: any) => {
-    var newArray = [] as any
-    var lookupObject = {} as any
-
-    originalArray = originalArray.map((item: any) => {
-      return {
-        ...item,
-        name: item.name + item.size,
-      }
-    })
-
-    for (var i in originalArray) {
-      lookupObject[originalArray[i][prop]] = originalArray[i]
-    }
-
-    for (i in lookupObject) {
-      const FilteredArray = originalArray.filter((orderItem: any) => {
-        return orderItem.name === lookupObject[i].name
-      })
-
-      const price = FilteredArray.reduce((prev: any, current: any) => {
-        return prev + +current.price
-      }, 0)
-
-      const count = FilteredArray.reduce((prev: any, current: any) => {
-        return prev + +1
-      }, 0)
-
-      newArray.push({
-        ...lookupObject[i],
-        price: price,
-        count: count,
-      })
-    }
-    const CleanArray = newArray.map((item: any) => {
-      return {
-        ...item,
-        name: item.name.replace(item.size, ""),
-      }
-    })
-
-    return CleanArray
-  }
 
   const FilteredBasketList = removeDuplicates(props.OrderItemsList, "name")
 
--- src/Components/GeneralScreens/Basket/Screens/PaymentScreen/PaymentScreen.tsx
+++ src/Components/GeneralScreens/Basket/Screens/PaymentScreen/PaymentScreen.tsx
@@ -1,7 +1,6 @@
 // PLUGINS IMPORTS //
 import React, { useState, useEffect } from "react"
-import { View, ScrollView, TouchableOpacity, StyleSheet } from "react-native"
-import Text from "~/Components/Shared/Components/Text/Text"
+import { ScrollView, StyleSheet } from "react-native"
 import PayButton from "~/Components/Shared/Components/PayButton/PayButton"
 
 // COMPONENTS IMPORTS //
@@ -10,6 +9,7 @@ import ProductsBasketList from "~/Components/Shared/Sections/ProductsBasketList/
 import OrderDetailsSection from "~/Components/Shared/Sections/OrderDetailsSection/OrderDetailsSection"
 
 // EXTRA IMPORTS //
+import { removeDuplicates } from "~/Components/Shared/Helpers/RemoveDuplicatesFunction"
 
 /////////////////////////////////////////////////////////////////////////////
 
@@ -42,50 +42,6 @@ type PropsType = {
 
 const PaymentScreen: React.FC<PropsType> = (props) => {
   const [totalPrice, setTotalPrice] = useState(0 as number)
-
-  const removeDuplicates = (originalArray: Array<any>, prop: any) => {
-    var newArray = [] as any
-    var lookupObject = {} as any
-
-    originalArray = originalArray.map((item: any) => {
-      return {
-        ...item,
-        name: item.name + item.size,
-      }
-    })
-
-    for (var i in originalArray) {
-      lookupObject[originalArray[i][prop]] = originalArray[i]
-    }
-
-    for (i in lookupObject) {
-      const FilteredArray = originalArray.filter((orderItem: any) => {
-        return orderItem.name === lookupObject[i].name
-      })
-
-      const price = FilteredArray.reduce((prev: any, current: any) => {
-        return prev + +current.price
-      }, 0)
-
-      const count = FilteredArray.reduce((prev: any, current: any) => {
-        return prev + +1
-      }, 0)
-
-      newArray.push({
-        ...lookupObject[i],
-        price: price,
-        count: count,
-      })
-    }
-    const CleanedArray = newArray.map((item: any) => {
-      return {
-        ...item,
-        name: item.name.replace(item.size, ""),
-      }
-    })
-
-    return CleanedArray
-  }
 
   useEffect(() => {
     setTotalPrice(
